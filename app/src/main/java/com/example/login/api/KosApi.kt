84c5326985ad7ba8367308726c2266b086a9f354
faithprayer <prayerfaith94@gmail.com>
2022-11-26 15:21:18 +0700

    library UI neomorphism login regis

--- app/src/main/java/com/example/login/api/KosApi.kt
+++ app/src/main/java/com/example/login/api/KosApi.kt
@@ -3,7 +3,7 @@ package com.example.login.api
 class KosApi {
     companion object {
         //sesuaikan ipv4 masing-masing
-        val BASE_URL = "http://192.168.1.249/PBP_API_LARAVEL/public/api/"
+        val BASE_URL = "http://192.168.100.171/PBP_API_LARAVEL/public/api/"
 
         val GET_ALL_URL = BASE_URL + "kos/"
         val GET_BY_ID_URL = BASE_URL + "kos/"
--- app/src/main/java/com/example/login/api/userApi.kt
+++ app/src/main/java/com/example/login/api/userApi.kt
@@ -4,7 +4,7 @@ class userApi {
 
     companion object{
         //sesuaikan ipv4 masing-masing
-        val BASE_URL = "http://192.168.1.249/PBP_API_LARAVEL/public/api/"
+        val BASE_URL = "http://192.168.100.171/PBP_API_LARAVEL/public/api/"
 
         val GET_BY_ID_URL = BASE_URL + "profilUser/"
         val UPDATE_USER = BASE_URL + "profilUser/"
