6deade28a27e59e70e2d31656258bced058f10e4
skysaurus13 <lukito2717@gmail.com>
2022-11-27 09:38:53 +0700

    Penerapan Library Bottom Navigator Pandora sebagai pengganti Bottom Navigation

--- app/src/main/java/com/example/login/HomeActivity.kt
+++ app/src/main/java/com/example/login/HomeActivity.kt
@@ -4,36 +4,57 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentController
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.bottomnavigation.BottomNavigationItemView
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView
-import com.example.login.databinding.ActivityMainBinding
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.pandora.bottomnavigator.BottomNavigator
+
+//import androidx.navigation.NavController
+//import androidx.fragment.app.Fragment
+//import androidx.fragment.app.FragmentController
+//import com.google.android.material.bottomnavigation.BottomNavigationView
+//import com.google.android.material.bottomnavigation.BottomNavigationItemView
+//import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+//import androidx.navigation.findNavController
+//import androidx.navigation.ui.AppBarConfiguration
+//import androidx.navigation.ui.setupActionBarWithNavController
+//import androidx.navigation.ui.setupWithNavController
+//import androidx.navigation.fragment.NavHostFragment
+//import androidx.navigation.ui.NavigationUI.setupWithNavController
+
 
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
+//    private lateinit var navController: NavController
+
     private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var navigator: BottomNavigator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
-        navController = navHostFragment.navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
-        setupWithNavController(bottomNavigationView, navController)
+        navigator = BottomNavigator.onCreate(
+            fragmentContainer = R.id.mainContainer,
+            bottomNavigationView = findViewById(R.id.bottomNavigation),
+            rootFragmentsFactory = mapOf(
+                    R.id.navigation_home to {FragmentKos()},
+                    R.id.navigation_chat to {ChatFragment()},
+                    R.id.navigation_pemesanan to {PemesananFragment()},
+                    R.id.navigation_profile to {ProfileFragment()}
+            ),
+            defaultTab = R.id.navigation_home,
+            activity = this
+        )
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+//        navController = navHostFragment.navController
+//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+//        setupWithNavController(bottomNavigationView, navController)
 
         sharedPreferences = getSharedPreferences("login", Context.MODE_PRIVATE)
 
+    }
+
+    override fun onBackPressed() {
+        if(!navigator.pop()){
+            super.onBackPressed()
+        }
     }
 
     fun getSharedPreferences() : SharedPreferences{
